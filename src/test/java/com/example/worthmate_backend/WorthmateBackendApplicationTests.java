75d105670e60b354b67deeb364081ce3e3c0a27a
DishantDrugkar <dishantdrugkar4@gmail.com>
2026-03-23 11:44:31 +0530

    Initial Commit

---
+++ src/test/java/com/example/worthmate_backend/WorthmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worthmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorthmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
